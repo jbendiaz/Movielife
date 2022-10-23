0e1ba88190838616472d1749941f3ada383b472d
Javier Diaz <javierdh49@gmail.com>
2022-10-23 15:01:23 -0400

    Segunda subida

--- index.py
+++ index.py
@@ -23,5 +23,53 @@ def registro():
 def noticia():
 	return  render_template("Noticia.html")
 
+@app.route("/warner")
+def warner():
+	return  render_template("Warner.html")
+
+@app.route("/marvel")
+def marvel():
+	return  render_template("Marvel.html")
+
+@app.route("/dc")
+def dc():
+	return  render_template("DC Comic.html")
+
+@app.route("/disney")
+def disney():
+	return  render_template("Disney.html")
+
+@app.route("/estrenos")
+def estrenos():
+	return  render_template("Estrenos.html")
+
+@app.route("/eventos")
+def eventos():
+	return  render_template("Eventos.html")
+
+@app.route("/warnere")
+def warnere():
+	return  render_template("WarnerE.html")
+
+@app.route("/marvele")
+def marvele():
+	return  render_template("MarvelE.html")
+
+@app.route("/disneye")
+def disneye():
+	return  render_template("DisneyE.html")
+
+@app.route("/dce")
+def dce():
+	return  render_template("DC ComicE.html")
+
+@app.route("/todas_las_peliculas")
+def tpeliculas():
+	return  render_template("Todas las pelis.html")
+
+@app.route("/peliculas_recientes")
+def rpeliculas():
+	return  render_template("Recientes.html")
+
 if __name__ == "__main__":
 	app.run(debug=True, port=5022)
