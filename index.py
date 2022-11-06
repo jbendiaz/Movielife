7b1f8ae04967186af903262f0a0abcad7e943f7c
Javier Diaz <javierdh49@gmail.com>
2022-11-06 11:36:37 -0400

    cuarta revision

--- index.py
+++ index.py
@@ -1,23 +1,127 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, jsonify, flash 
+from flask_mysqldb import MySQL
+from config import config 
+from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager, login_user, logout_user, login_required
+
+# Models:
+from models.ModelUser import ModelUser
+
+# Entities:
+from models.entities.User import User 
 
 app = Flask(__name__)
-"""
-@app.route("/")
-def principal():
-	return "Bienvenido, pasen ahora"
 
-@app.route("/contactos")
-def contactos():
-	return "Pagina de contactos"
-"""
+csrf = CSRFProtect()
+db = MySQL(app)
+login_manager_app = LoginManager(app)
+
+@app.before_request
+def before_request():
+	print("Antes de...")
+
+@app.after_request
+def after_request(response):
+	print("Despues de...")
+	return response
+
+@app.route("/login/inicio")
+def indexlogin():
+	return  render_template("/login/index.html")
+
+@app.route("/login/noticia")
+def noticialogin():
+	return  render_template("/login/Noticia.html")
+
+@app.route("/login/warner")
+def warnerlogin():
+	return  render_template("/login/Warner.html")
+
+@app.route("/login/marvel")
+def marvelogin():
+	return  render_template("/login/Marvel.html")
+
+@app.route("/login/dc")
+def dclogin():
+	return  render_template("/login/DC Comic.html")
+
+@app.route("/login/disney")
+def disneylogin():
+	return  render_template("/login/Disney.html")
+
+@app.route("/login/estrenos")
+def estrenoslogin():
+	return  render_template("/login/Estrenos.html")
+
+@app.route("/login/eventos")
+def eventoslogin():
+	return  render_template("/login/Eventos.html")
+
+@app.route("/login/warnere")
+def warnerelogin():
+	return  render_template("/login/WarnerE.html")
+
+@app.route("/login/marvele")
+def marvelelogin():
+	return  render_template("/login/MarvelE.html")
+
+@app.route("/login/disneye")
+def disneyelogin():
+	return  render_template("/login/DisneyE.html")
+
+@app.route("/login/dce")
+def dcelogin():
+	return  render_template("/login/DC ComicE.html")
+
+@app.route("/login/todas_las_peliculas")
+def tpeliculaslogin():
+	return  render_template("/login/Todas las pelis.html")
+
+@app.route("/login/peliculas_recientes")
+def rpeliculaslogin():
+	return  render_template("/login/Recientes.html")
+
+
+
+
+@login_manager_app.user_loader
+def load_user(id):
+    return ModelUser.get_by_id(db, id)
 
 @app.route("/")
+def index():
+	return redirect(url_for("principal"))
+
+@app.route("/index", methods=["GET", "POST"])
 def principal():
-	return  render_template("index.html")
+	if request.method == "POST":
+		user = User(0, request.form["username"], request.form["password"])
+		logged_user = ModelUser.principal(db, user)
+		if logged_user != None:
+			if logged_user.password:
+				login_user(logged_user)
+				return redirect(url_for('indexlogin'))
+			else:
+				flash("CLave Incorrecta")
+				return render_template("index.html")
+		else:
+			flash("Error de Usuario")
+			return render_template("index.html")
+	else:
+		return  render_template("index.html")
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('principal'))
 
 @app.route("/registro")
 def registro():
 	return  render_template("Registro.html")
+
+@app.route("/inicio_de_sesion")
+def inicio():
+	return  render_template("login.html")
 
 @app.route("/noticia")
 def noticia():
@@ -69,7 +173,40 @@ def tpeliculas():
 
 @app.route("/peliculas_recientes")
 def rpeliculas():
-	return  render_template("Recientes.html")
+	return  render_template("Recientes.html")	
+
+
+
+
+def query_string():
+	print(request)
+	print(request.args)
+	print(request.args.get("param1"))
+	return "ok"
+
+#@app.route("/usuario")
+#def list_usuario():
+#	data={}
+#	try:
+#		cursor = conexion.connection.cursor()
+#		slq = "SELECT * FROM `usuario` WHERE 1"
+#		cursor.execute(sql)
+#		usuario = cursor.fetchall()
+#		data["mensaje"] = "Excelente..."
+#	except Exception as ex:
+#		data["mensaje"] = "Error..."
+#	return jsonify(data)
+
+def status_404(error):
+	#return render_template("404.html"), 404
+	return redirect(url_for("index"))
+
+#def status_405(error):
+    #return redirect(url_for('principal'))
 
 if __name__ == "__main__":
-	app.run(debug=True, port=5022)
+	app.config.from_object(config['development'])
+	app.add_url_rule("/query_string", view_func=query_string)
+	#app.register_error_handler(405, status_405)
+	app.register_error_handler(404, status_404)
+	app.run(port=5022)
